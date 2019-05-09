d2a0f98f626978a24aa16b0f717a709c7475357f
torsina <theo.sagot.1@gmail.com>
2019-05-09 23:28:00 +0200

    add global required env

--- index.js
+++ index.js
@@ -1,8 +1,9 @@
 const path = require("path");
 const fs = require("fs");
 
-const envConfig = JSON.parse(fs.readFileSync(path.join(__dirname, "../../../config/env.json")));
-
+let envConfig = JSON.parse(fs.readFileSync(path.join(__dirname, "../../../config/env.json")));
+const globalEnvConfig = JSON.parse(fs.readFileSync("./env.json"));
+envConfig = envConfig.concat(globalEnvConfig);
 for(let i = 0, n = envConfig.length; i < n; i++) {
     if(process.env[envConfig[i]] === "") throw new Error(`The environment variable ${envConfig[i]} is empty`);
 }
