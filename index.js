033a99ff483185ad9f7115f741e2cd54a9bcb1dd
torsina <theo.sagot.1@gmail.com>
2019-05-15 00:21:17 +0200

    send a single error with all the missing variables, version bump

--- index.js
+++ index.js
@@ -5,9 +5,11 @@ module.exports = () => {
     let envConfig = JSON.parse(fs.readFileSync(path.join(__dirname, "../../../config/env.json")));
     const globalEnvConfig = JSON.parse(fs.readFileSync("./node_modules/@arys/required-env/env.json"));
     envConfig = envConfig.concat(globalEnvConfig);
+    const missingVariables = [];
     for(let i = 0, n = envConfig.length; i < n; i++) {
         if(process.env[envConfig[i]] === "" || process.env[envConfig[i]] === undefined) {
-            throw new Error(`The environment variable ${envConfig[i]} is empty`);
+            missingVariables.push(envConfig[i]);
         }
     }
+    if(missingVariables.length !== 0) throw new Error(`The following environment variables are empty: ${envConfig}`);
 };
