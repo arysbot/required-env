26161e8a44915bb352f009c0c25a2bd7eafdec49
torsina <theo.sagot.1@gmail.com>
2019-05-15 00:14:00 +0200

    fix to include undeclared variables

--- index.js
+++ index.js
@@ -6,6 +6,8 @@ module.exports = () => {
     const globalEnvConfig = JSON.parse(fs.readFileSync("./node_modules/@arys/required-env/env.json"));
     envConfig = envConfig.concat(globalEnvConfig);
     for(let i = 0, n = envConfig.length; i < n; i++) {
-        if(process.env[envConfig[i]] === "") throw new Error(`The environment variable ${envConfig[i]} is empty`);
+        if(process.env[envConfig[i]] === "" || process.env[envConfig[i]] === undefined) {
+            throw new Error(`The environment variable ${envConfig[i]} is empty`);
+        }
     }
 };
