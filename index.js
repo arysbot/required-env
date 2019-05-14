3b5371f42ea090fc9f58b3e9b929ecadfaeec5d3
torsina <theo.sagot.1@gmail.com>
2019-05-15 00:10:35 +0200

    version bump, switch to exported function instead of trash

--- index.js
+++ index.js
@@ -1,9 +1,11 @@
 const path = require("path");
 const fs = require("fs");
 
-let envConfig = JSON.parse(fs.readFileSync(path.join(__dirname, "../../../config/env.json")));
-const globalEnvConfig = JSON.parse(fs.readFileSync("./env.json"));
-envConfig = envConfig.concat(globalEnvConfig);
-for(let i = 0, n = envConfig.length; i < n; i++) {
-    if(process.env[envConfig[i]] === "") throw new Error(`The environment variable ${envConfig[i]} is empty`);
-}
+module.exports = () => {
+    let envConfig = JSON.parse(fs.readFileSync(path.join(__dirname, "../../../config/env.json")));
+    const globalEnvConfig = JSON.parse(fs.readFileSync("./node_modules/@arys/required-env/env.json"));
+    envConfig = envConfig.concat(globalEnvConfig);
+    for(let i = 0, n = envConfig.length; i < n; i++) {
+        if(process.env[envConfig[i]] === "") throw new Error(`The environment variable ${envConfig[i]} is empty`);
+    }
+};
